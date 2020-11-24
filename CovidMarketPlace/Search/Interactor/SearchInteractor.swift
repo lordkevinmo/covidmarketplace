51cdfa8a2871356ef51d9ed1c624992cceba1992
AGBENYA Koffi Moïse <kevagb03@gmail.com>
2020-11-24 23:41:28 +0100

    init shop home and search screens

---
+++ CovidMarketPlace/Search/Interactor/SearchInteractor.swift
@@ -0,0 +1,8 @@
+//
+//  SearchInteractor.swift
+//  CovidMarketPlace
+//
+//  Created by Moïse AGBENYA on 24/11/2020.
+//
+
+import Foundation
---
+++ CovidMarketPlace/Search/View/SearchView.swift
@@ -0,0 +1,20 @@
+//
+//  SearchView.swift
+//  CovidMarketPlace
+//
+//  Created by Moïse AGBENYA on 24/11/2020.
+//
+
+import UIKit
+
+class SearchView: UIViewController {
+    @IBOutlet weak var searchbar: UISearchBar!
+    @IBOutlet weak var container: UIView!
+    
+    static let storyboardID = "search-view"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
