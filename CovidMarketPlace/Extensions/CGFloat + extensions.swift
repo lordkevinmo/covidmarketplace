b747a81a749c5c4eb4598f493fff88c38dac80ea
AGBENYA Koffi Moïse <kevagb03@gmail.com>
2020-11-27 22:41:21 +0100

    prepare some features to deal with new screen

---
+++ CovidMarketPlace/Extensions/CGFloat + extensions.swift
@@ -0,0 +1,12 @@
+//
+//  CGFloat + extensions.swift
+//  CovidMarketPlace
+//
+//  Created by Moïse AGBENYA on 27/11/2020.
+//
+
+import UIKit
+
+extension CGFloat {
+    static let minimumSpacing: CGFloat = 8
+}
