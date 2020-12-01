da518387a231d8d993b6622578c6e29d1c4ee32c
AGBENYA Koffi Moïse <kevagb03@gmail.com>
2020-12-01 23:37:59 +0100

    organize product details screen

---
+++ CovidMarketPlace/Shop/View/cells/ProductDescription.swift
@@ -0,0 +1,20 @@
+//
+//  ProductDescription.swift
+//  CovidMarketPlace
+//
+//  Created by Moïse AGBENYA on 01/12/2020.
+//
+
+import UIKit
+
+class ProductDescription: UICollectionViewCell {
+    @IBOutlet weak var productDescription: UILabel!
+    
+    static let reuseIdentifier = "product-description"
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
